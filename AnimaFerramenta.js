ecc70e1e337db4fc82386525af838a4661d8b96b
João Vitor Alves Ribeiro <joao.ribeiro@phb.com>
2023-09-06 15:11:28 -0300

    versão beta

--- AnimaFerramenta.js
+++ AnimaFerramenta.js
@@ -1,10 +1,6 @@
 let area = document.getElementById('ff')
 let divCentro = document.getElementById('ferramen')
-let teste1
-let teste2
-let teste3
-let pos
-let telaMax
+let img1
 let intervalID 
 let i = 0
 
@@ -14,48 +10,143 @@ const img = [
 "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/java/java-original-wordmark.svg",
 "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/csharp/csharp-original.svg",
 "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/javascript/javascript-original.svg",
+"https://cdn.jsdelivr.net/gh/devicons/devicon/icons/c/c-original.svg",
+
 "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/spring/spring-original-wordmark.svg",
 "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/nodejs/nodejs-original.svg",
-'https://cdn.jsdelivr.net/gh/devicons/devicon/icons/postgresql/postgresql-original-wordmark.svg',
+"https://cdn.jsdelivr.net/gh/devicons/devicon/icons/typescript/typescript-original.svg",
+"https://cdn.jsdelivr.net/gh/devicons/devicon/icons/react/react-original.svg",
+
+
+
+"https://cdn.jsdelivr.net/gh/devicons/devicon/icons/postgresql/postgresql-original-wordmark.svg",
 "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/mysql/mysql-original-wordmark.svg",
+"https://cdn.jsdelivr.net/gh/devicons/devicon/icons/mongodb/mongodb-original-wordmark.svg",
+
+"https://cdn.jsdelivr.net/gh/devicons/devicon/icons/git/git-original-wordmark.svg",
+"https://cdn.jsdelivr.net/gh/devicons/devicon/icons/apache/apache-original-wordmark.svg",
 "fotos/noun-kanban-board-4759118.svg",
+"fotos/noun-agile-development-4759133.svg",
 
-"fotos/noun-agile-development-4759133.svg"
+
 ]
-const elementoImg = [teste1, teste2, teste3]
-console.log(`tamanho da div${divCentro.style.innerWidth}`)
-function init(){
+const elementoImg = [img1]
 
+let div
+let opcoes = 4
+let titulo
+let info1
+let info2
+
+
+
+
+
+
+
+
+
+function init(){
+if(opcoes <= img.length){
+titulo = document.createElement('h3')
+info1 = document.createElement('p')
+info2 = document.createElement('p')
+div = document.createElement('div')
+titulos()
+
+for(i;i<opcoes;i++){
 elementoImg[i] = document.createElement('img')
 elementoImg[i].setAttribute('src', img[i])
 elementoImg[i].setAttribute('class', 'imgF')
-area.appendChild(elementoImg[i])
-telaMax=window.innerWidth
-console.log(`tamanho da tela : ${telaMax}`)
-elementoImg[i].style="position:relative;left:-400px"
-if (!intervalID) {
-    intervalID = setInterval(myCallback, 10, pos, elementoImg[i]);
 
-  }
-console.log(`valor de i e q quantidade de voltas ${i}`)
+div.appendChild(elementoImg[i])
+
 }
-function myCallback(pos, elementoImg){
+div.appendChild(info1)
+div.appendChild(info2)
+area.appendChild(div)
+div.style.padding = '10vh'
+div.style.height = '50vh'
+if (!intervalID) {
+    intervalID = setInterval(myCallback, 1000);
+  } 
+}else{   
+    console.log("acabou as imagens ")
+ }
+ 
+}
+
+function titulos(){
+    if(opcoes == 4){
+        titulo.innerHTML = 'Lnguagens de prgramação'
+        info1.innerHTML = `OOP(Object-Oriented Programming) - SGBD - Collections - Servlet  `
+        info2.innerHTML = 'JSP - JPA - Intellij - Eclipse - Visual Studio - VS CODE '
+        div.appendChild(titulo)
+    }else if(opcoes == 8){
+        titulo.innerHTML = 'fermeworks'
+        info1.innerHTML = 'MVC - Protocolo HTTP, DNS, TCP/IP'
+        info2.innerHTML = 'JUnit - .NET CORE - ASP.NET'
+        
+        div.appendChild(titulo)
+    }
+    else if(opcoes == 11){
+        titulo.innerHTML = 'banco de dados'
+        info1.innerHTML = 'MVC - Protocolo HTTP, DNS, TCP/IP'
+        info2.innerHTML = 'JUnit - .NET CORE - ASP.NET'
+        div.appendChild(titulo)
+    }
+    else if(opcoes == 15){
+        titulo.innerHTML = 'DevOps'
+        info1.innerHTML = 'MVC - Protocolo HTTP, DNS, TCP/IP'
+        info2.innerHTML = 'JUnit - .NET CORE - ASP.NET'
+        div.appendChild(titulo)
+    }
+}
+
+function myCallback(){
+    if(opcoes == img.length){
+        console.log("função chamada")
+        opcoes = 4
+        i = 0
+        area.removeChild(div)
+        init()
+    }else{
+        console.log("função chamada")
+        area.removeChild(div)
+        if(opcoes == 8){
+            opcoes = opcoes + 3
+        }else{
+            opcoes = opcoes + 4
+        }
+        
+        
+        init()
+    }
+    
+        
+}
+
+
+window.onload=init()
+
+
+/* function myCallback(pos, elementoImg){
     
     pos=parseInt(elementoImg.style.left)
     pos++
-    elementoImg.style=`position:relative;left:${pos}px`
+    elementoImg.style=`position:relative;left:${pos}%`
     
-    if(pos == telaMax - 400){ 
+    if(pos == telaMax){ 
         clearInterval(intervalID);
         intervalID = null;
         area.removeChild(elementoImg)
         i = i + 1
+        elementoImg.style=`position:relative;left:${0}%`
         init()
     }
     
-}
+} */
 
 
 
 
-window.onload=init()
