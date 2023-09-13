d42434861b69ea2380fd3f27a8d61ff61b7303ef
unknown <jvalves217@gmail.com>
2023-09-12 22:23:21 -0300

    texto area projeto

--- InteracaoProjetos.js
+++ InteracaoProjetos.js
@@ -75,44 +75,27 @@ function projeto1(){
 
     elementoTitulo.innerHTML = 
     `
-    <h1>Artigos IoT</h1>
+    <h2>Artigos IoT &#x1F4DC</h2>
     <hr color="#023554">
     <br>
-    <p>Durante meu curso de eletronica pude ter a oportunidade em desenvolver uma material didatico, voltado a introdução ao microcontrolador PIC, 
+    <p class="textoProjeto">Durante meu curso de eletronica pude ter a oportunidade em desenvolver uma material didatico, voltado a introdução ao microcontrolador PIC, 
     sempre senti dificuldade de encontrar materiais bons e didatico sobre tal tema, portanto decidir desenvolver o meu prorio material.
     <br>
     Em parceria com a loja eletrogate puder ter exedo nesse meu desejo. 
     </p>
     <br>
-    <h2>Introdução ao microcontrolador PIC</h2>
+    <h3>Introdução ao microcontrolador PIC &#x1F4E0</h3>
     <hr color="#023554">
     <br>
-    <p>Nesse artigo podemos encontrar a base da teoria atras do componete e a analise do seu datashed 
+    <p class="textoProjeto">Nesse artigo podemos encontrar a base da teoria atras do componete e a analise do seu datashed 
     <br>
     Para acessar ao artigo completo basta apenas clicar aqui :  <a class="linkss" href="https://blog.eletrogate.com/introducao-aos-microcontroladores-pic-parte-1/">Click aqui para acessar</a>
     </p>
     <br>
     <img src="fotos/pic.webp"  align="center" alt="" width="320px" height="180px">
     <br>
-    <h2>Introdução ao microcontrolador PIC</h2>
-    <hr color="#023554">
-    <br>
-    <p>Nesse artigo podemos encontrar a base da teoria atras do componete e a analise do seu datashed 
-    <br>
-    Para acessar ao artigo completo basta apenas clicar aqui :  <a class="linkss" href="https://blog.eletrogate.com/introducao-aos-microcontroladores-pic-parte-1/">Click aqui para acessar</a>
-    </p>
-    <br>
-    <img src="fotos/pic.webp"  align="center" alt="" width="320px" height="180px">
-    <br>
-    <h2>Introdução ao microcontrolador PIC</h2>
-    <hr color="#023554">
-    <br>
-    <p>Nesse artigo podemos encontrar a base da teoria atras do componete e a analise do seu datashed 
-    <br>
-    Para acessar ao artigo completo basta apenas clicar aqui :  <a class="linkss" href="https://blog.eletrogate.com/introducao-aos-microcontroladores-pic-parte-1/">Click aqui para acessar</a>
-    </p>
-    <br>
-    <img src="fotos/pic.webp"  align="center" alt="" width="320px" height="180px">
+    
+   
   
     `
     
